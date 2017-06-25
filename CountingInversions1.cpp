4dd0a1a8115555ece9dedb66181c864dd66629cf
ankit1998 <ankitsnigam@gmail.com>
2017-06-25 22:43:48 +0530

    Update CountingInversions1.cpp

--- CountingInversions1.cpp
+++ CountingInversions1.cpp
@@ -8,7 +8,7 @@ typedef long long ll;
  
 ll _merge(int a[], int tmp[], int l, int mid, int r) {
   int i, j, k;
-  ll inv = 0; //i for left, j for right
+  ll inv = 0; //i for left subarray, j for rightsubarray
   i = k = l; 
   j = mid;
   while ((i < mid) && (j <= r)) {
